92d7382ef59631d68c170741d7d0ec3cf98b96cb
davidlopez <dfl11@alu.ua.es>
2023-07-10 21:05:46 +0200

    jni

--- app/src/main/java/es/ua/eps/cannyandroid/MainActivity.java
+++ app/src/main/java/es/ua/eps/cannyandroid/MainActivity.java
@@ -3,8 +3,20 @@ package es.ua.eps.cannyandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import org.opencv.android.OpenCVLoader;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static String TAG = "MainActivity";
+
+    static {
+        if (OpenCVLoader.initDebug()) {
+            Log.i(TAG, "OpenCV Loaded!!!!");
+        }
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
